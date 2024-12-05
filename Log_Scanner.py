1bdcb36527f24934b3b4dcd2b7221ec1f7798bce
Anshu Gupta <77850344+Anshu370@users.noreply.github.com>
2024-12-06 01:02:02 +0530

    other two major function updated
    updated : frequently_Accessed_Endpoint added functionality of checking frequency of Accessed Endpoint and detect_Suspicious_Activity added functionality of Detecting failed login attempts tried by user.

--- Log_Scanner.py
+++ Log_Scanner.py
@@ -17,19 +17,51 @@ def count_Request(file):
     return counter_DataFrame
 
 def frequently_Accessed_Endpoint(file):
-    pass
+    counter = {}
+    with open(file, 'r') as sample:
+        data = sample.readlines()
+        for log in data:
+            log = list(log.split(" "))
+            if log[6] in counter:
+                counter[log[6]] += 1
+            else:
+                counter[log[6]] = 1
 
-def detect_Suspicious_Activity(file):
-    pass
+    endpoint = ""
+    frequency = 0
+
+    for i in counter:
+        if counter[i] > frequency:
+            frequency = counter[i]
+            endpoint = i
+
+    statement = f"Most Frequently Accessed Endpoint: {endpoint} (Accessed {frequency} times)"
+
+    return statement
+
+def detect_Suspicious_Activity(file, login_threshold = 10):
+    counter = {}
+
+    with open(file, 'r') as sample:
+        data = sample.readlines()
+        for log in data:
+            log = list(log.split(" "))
+            if '401' in log or ('"Invalid' in log and 'credentials"' in log):
+                if log[0] in counter:
+                    counter[log[0]] += 1
+                else:
+                    counter[log[0]] = 1
+        counter_DataFrame = pd.DataFrame([[ip, counter[ip]] for ip in counter if counter[ip] > login_threshold], columns=['IP Address', 'Failed Login Attempts'])
+        if len(counter_DataFrame) == 0:
+            return "No Failed IP addresses with login attempts exceeding a threshold"
+    return counter_DataFrame
 
 def generate_CSV(file):
     return True
 
 def main():
 
-    file = sys.argv[1]
-
-    if file in ["help", "-h", "--H"] or len(sys.argv) < 1:
+    if len(sys.argv) == 1:
         print("'-i' '--I' To get the Count Requests per IP Address")
         print("'-f' '--F' To get the Identify the Most Frequently Accessed Endpoint")
         print("'-s' '--S' To Detect Suspicious Activity(Failed Login)")
@@ -39,6 +71,8 @@ def main():
         print("python Log_Scanner.py <file location> <tag>")
         print("Example :- python Log_Scanner.py './sample.log' -i")
         sys.exit(1)
+
+    file = sys.argv[1]
 
     if len(sys.argv) > 2:
         tag = sys.argv[2]
@@ -51,24 +85,21 @@ def main():
             print(frequent_Endpoint)
 
         elif tag in ['-a', '--A']:
-            suspicious_activity = detect_Suspicious_Activity(file)
+            if len(sys.argv) <= 3:
+                print("Please enter the Failed Login Threshold Value")
+                sys.exit(1)
+
+            suspicious_activity = detect_Suspicious_Activity(file, int(sys.argv[3]))
             print(suspicious_activity)
+
         elif tag in ['-c', '--C']:
             if generate_CSV(file):
                 print("log_analysis_data.log file generated successfully")
             else:
                 print("Some error File not created")
+
         else:
-            request_counter = count_Request(file)
-            frequent_Endpoint = frequently_Accessed_Endpoint(file)
-            suspicious_activity = detect_Suspicious_Activity(file)
-            if generate_CSV(file):
-                print("log_analysis_data.log file generated successfully")
-                print(request_counter)
-                print(frequent_Endpoint)
-                print(suspicious_activity)
-            else:
-                print("Some error File not created")
+            print("Try using python Log_Scanner.py -> To open Help menu")
         sys.exit(1)
     else:
         if generate_CSV(file):
@@ -82,4 +113,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # count_Request('./sample.log')
+
